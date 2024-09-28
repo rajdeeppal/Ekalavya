bc2c6ea0e33dc1c5c487d02072d3ada91d5b8d94
Debjit Chattopadhyay <debjitchatterjee31@gmail.com>
2024-09-28 21:17:40 +0530

    Protecting user endpoints

--- src/main/java/com/ekalavya/org/config/SecurityConfig.java
+++ src/main/java/com/ekalavya/org/config/SecurityConfig.java
@@ -38,8 +38,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/self-service/**").permitAll()
-                        .requestMatchers("/user/**").permitAll()
-                        .requestMatchers("/roleAudit/**").permitAll()
                         .requestMatchers("/admin/sendOtp", "/admin/validateOtp").permitAll()
                         .requestMatchers("/beneficiary/**").permitAll() //TODO:  to be removed later once, login ui is implemented
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
