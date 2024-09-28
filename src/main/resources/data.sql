be9675f19b38afabe5a35492df80259ae89d42c8
Debjit Chattopadhyay <debjitchatterjee31@gmail.com>
2024-09-28 13:11:24 +0530

    Database setup steps

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,14 @@
+--- after performing Database refresh or after initial project setup, execute the below
+--- queries in this order
+
 INSERT INTO role (name) VALUES ('PM');
 INSERT INTO role (name) VALUES ('DOMAIN EXPERT');
 INSERT INTO role (name) VALUES ('TRUSTEE');
 INSERT INTO role (name) VALUES ('CEO');
 INSERT INTO role (name) VALUES ('AO');
+INSERT INTO role (name) VALUES ('EADMIN');
 
-INSERT INTO admin (email, username, is_already_validated)
-VALUES ('rajdeep108pal@gmail.com', 'Admin', false);
-
-
-
--- Beneficiaries Init-----
-
-select * from m_beneficiary;
-select * from m_component;
-select * from m_activity;
-select * from m_task;
-select * from m_task_update;
+--- create a user from swagger
 
 select * from project;
 
@@ -26,10 +19,6 @@ VALUES
 (3, 'Airtel', 'N', 1)
 
 select * from users;
-
-INSERT into users(id, domain, emailid, emp_id, is_active, password, username) VALUES
-(1, 'domain1', 'debjit16.dc@gmail.com', 1898635, 'Y',
-'$2a$12$Jt47x27jaRLMOcokEygMeuQToIvROdaS4QpmRoIiwnAM5XxZjLRPe','debjit31')
 
 
 
